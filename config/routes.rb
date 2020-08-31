33ac63445aa4a752540971719e319e372ff8d542
datemaki-maki <68716930+datemaki-maki@users.noreply.github.com>
2020-09-01 06:32:23 +0900

    Revert "グループの作成"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
+  root "messages#index"
 end
